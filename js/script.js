398f4dbdd4b8483ce1fd51865ae6fb5c20d8d690
End-front <62587226+End-front@users.noreply.github.com>
2020-03-27 14:01:34 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
 function ibg(){
-let ibg=document.querySelectorAll(".ibg"); for (var i = 0; i < ibg.length; i++) { if(ibg[i].querySelector('img')){ ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')'; } }
+	let ibg=document.querySelectorAll(".ibg"); for (var i = 0; i < ibg.length; i++) { if(ibg[i].querySelector('img')){ ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')'; } }
 
 }
 ibg();
 
+function autoheight(){
+	var width = $('.block__tiels__img').width();
+	$('.block__tiels__img').height(width);
+} autoheight();
+
+
+$(window).resize(function() {
+	autoheight();
+});
+
 $(document).ready(function() {
-            $('.owl-carousel').owlCarousel({
-            	items: 1,
-            	loop: true,
-                autoplay: true,
-                autoplayTimeout: 8000,
-                autoplaySpeed: 800,
-            });
-        });
+	$('.owl-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 8000,
+		autoplaySpeed: 800,
+		autoplayHoverPause: true,
+	});           
+	$('.owl-carousel-1').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 10000,
+		autoplaySpeed: 800,
+		autoplayHoverPause: true,
+		nav: false,
+	});
+	$.fn.equivalent = function (){
+	        var $blocks = $(this),
+	            maxH    = $blocks.eq(0).height();
+	        $blocks.each(function(){
+	            maxH = ( $(this).height() > maxH ) ? $(this).height() : maxH;
+	        });
+	        $blocks.height(maxH);
+	    }
+	    $('#equivalent .block__carousel__item__line').equivalent();             
+});
